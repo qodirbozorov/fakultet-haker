62584f7a49fd20fce8c842ec7ecb806ef46c32f9
Qodir Bozorov <abduqodirnote@gmail.com>
2022-09-14 18:41:04 +0500

    final

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open('talabalar.json') as json_file:
 	data = json.load(json_file)
 
 	# Print the type of data variable
-	print("Type:", type(data), "🏃‍♂️Running ...")
+	print("Type:", type(data), "⌛Running ...")
 #inline keybordni va knopkalarni chaqirdim
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, Filters, ConversationHandler, MessageHandler)
@@ -25,16 +25,16 @@ def name_checker(update, context):
   for i in data:
     if text.lower() in i["Toliq ismi"].lower():
       indexim += 1
-      time.sleep(2)
+      time.sleep(1)
       update.message.reply_html(f" Natija soni: {indexim}\n \n👤 <b>To‘liq ismi:</> <i>  {i['Toliq ismi'].title()}</i> \n \n | 👥 <b>Guruh:</b> <i>{i['Guruh']}</i> \n | 📍 <b>Manzil: </b>  {i['Viloyat']} {i['Tuman']} \n | 🎂 <b>Tug‘ilgan sanasi:</b>  <i> {i['Tugilgan sana']}</i> \n | ℹ️ <b>Fuqaroligi: </b> {i['Fuqarolik']} \n \n | <b>PINFL: </b><code> {i['JSHSHIR-kod']} </code> \n | <b>Passport: </b><code>{i['Pasport']}</code>\n \n @faculty_cognitobot")
-
-
-def checker():
-  name = input("Ismni kiriting: ")
-  son = 0
-  for i in data:
-    if name.lower() in i["Toliq ismi"].lower():
-      print(f'\n {i["Toliq ismi"]} \n {i["Guruh"]} \n')
+  if indexim == 0:
+    update.message.reply_html("\n 🧐 Natija topilmadi!\n \n ✳️ Ismni tekshirib qaytadan harakat qilib ko'ring!")
+  # def checker():
+#   name = input("Ismni kiriting: ")
+#   son = 0
+#   for i in data:
+#     if name.lower() in i["Toliq ismi"].lower():
+#       print(f'\n {i["Toliq ismi"]} \n {i["Guruh"]} \n')
 
  
 def main():
